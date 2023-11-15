d345ba7a158fed06dc3ff1c8fe5ee9f20b9894a2
slooi <s.looi2@outlook.com>
2023-11-15 15:07:44 +0000

    Imeplement barebones validation and transform plus /get-car-value route

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import {determineCarValue} from "./api"
+import {z} from "zod"
 
 const PORT = 8080
 
@@ -7,17 +8,36 @@ const app = express()
 app.get("/",(req,res)=>{
 	res.send("hi")
 })
+const SchemaGetCarValue = z.object({
+	model: z.string(),
+	year: z.string().refine((stringNumber)=>{
+		const numberOrNan = Number(stringNumber);
+		return !isNaN(numberOrNan) && numberOrNan>0
+	}).transform((stringNumber) => {
+		return Number(stringNumber)
+	}),
+});
 
-app.get("/api",(req,res)=>{
-	res.json({test:"hi"})
-})
 app.get("/api/get-car-value",(req,res)=>{
-	res.json({test:"hi"})
+	try{
+		const result = SchemaGetCarValue.parse(req.query)
+		console.log(result.model)
+		console.log(result.year)
+		console.log(result.model)
+		console.log(result.year)
+		console.log(typeof result.model)
+		console.log(typeof result.year)
+	}catch{
+		res.json({error:"there was an error"})
+	}
 })
 
-app.get("/api/get-car-value2",(req,res)=>{
-	res.json({test:"hi",test2:"hi"})
-})
+// app.get("/api",(req,res)=>{
+// 	res.json({test:"hi"})
+// })
+// app.get("/api/get-car-value2",(req,res)=>{
+// 	res.json({test:"hi",test2:"hi"})
+// })
 
 app.listen(PORT,()=>{
 	console.log("Listening on port "+PORT)
