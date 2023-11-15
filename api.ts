66d5ac2535c643d517da0fc45c283c615a741172
s <s.looi2@outlook.com>
2023-11-16 01:10:22 +1300

    Implement determineCarValue and getAlphabetPosition

---
+++ api.ts
@@ -0,0 +1,36 @@
+export const ERROR_ARGUMENT_LENGTH_NOT_ONE = new Error("An argument with length > 1 was used as a parameter! This is NOT allowed!")
+export const ERROR_MODEL_NOT_SPECIFIED = new Error("No model was specified!")
+export const ERROR_NEGATIVE_YEAR = new Error("Negative years are NOT accepted.")
+
+export function determineCarValue(model:string,year:number):number{
+	if (model.length === 0){
+		throw ERROR_MODEL_NOT_SPECIFIED
+	}
+	if (year<0){
+		throw ERROR_NEGATIVE_YEAR
+	}
+
+	let carValue:number = 0
+
+	// Calculate car value from alphabet position
+	for(let i=0;i<model.length;i++){
+		carValue += getAlphabetPosition(model[i]) * 100
+	}
+
+	// Add year price
+	carValue += year
+
+	// RETURN
+	return carValue
+}
+
+export function getAlphabetPosition(letter:string){
+	if (letter.length > 1) throw ERROR_ARGUMENT_LENGTH_NOT_ONE
+
+	const charCode:number = letter.toLowerCase().charCodeAt(0)
+	if (charCode >= 97 && charCode <= 97+25){
+		return charCode-96
+	}
+
+	return 0
+}
