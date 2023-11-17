aa827b0eb1979da0397f0eb02ca455affc32d39a
slooi <s.looi2@outlook.com>
2023-11-17 14:01:11 +0000

    Make naming consistent

--- src/routes/r_api.ts
+++ src/routes/r_api.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import c_EstimateCarValue from "../controllers/c_estimateCarValue";
+import c_estimateCarValue from "../controllers/c_estimateCarValue";
 
 const r_api = express.Router()
 
 // Routes -> Controllers
-r_api.get("/estimate-car-value",c_EstimateCarValue)
+r_api.get("/estimate-car-value",c_estimateCarValue)
 
 
 
