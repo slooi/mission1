5b0fb331565fb26aca61f906fdb19cf8b216893c
slooi <s.looi2@outlook.com>
2023-11-17 14:20:18 +0000

    Rename

--- src/controllers/c_estimateCarValue.ts
+++ src/controllers/c_estimateCarValue.ts
@@ -1,12 +1,12 @@
 import {Request, Response} from "express";
-import determineCarValue from "../services/s_estimateCarValue";
+import s_estimateCarValue from "../services/s_estimateCarValue";
 import v_estimateCarValue from "../validators/v_estimateCarValue";
 
 
 const c_EstimateCarValue = (req:Request,res:Response)=>{
 	try{
 		const validatedQuery = v_estimateCarValue.parse(req.query)
-		const carValue = determineCarValue(validatedQuery.model,validatedQuery.year)
+		const carValue = s_estimateCarValue(validatedQuery.model,validatedQuery.year)
 		res.json({car_value:carValue})
 	}catch{
 		res.json({error:"there was an error"})
