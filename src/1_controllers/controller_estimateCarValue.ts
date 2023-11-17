0c70681f7670df245198552181ae670608991ee0
slooi <s.looi2@outlook.com>
2023-11-17 16:52:01 +0000

    Rename apiFeatures into features

--- src/1_controllers/controller_estimateCarValue.ts
+++ src/1_controllers/controller_estimateCarValue.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from "express";
-import s_estimateCarValue from "../1A_apiFeatures/estimateCarValue/service_estimateCarValue";
-import v_estimateCarValue from "../1A_apiFeatures/estimateCarValue/validator_estimateCarValue";
+import s_estimateCarValue from "../2_features/estimateCarValue/service_estimateCarValue";
+import v_estimateCarValue from "../2_features/estimateCarValue/validator_estimateCarValue";
 
 
 const c_EstimateCarValue = (req:Request,res:Response)=>{
